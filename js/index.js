228016b01b148f2d9c2c1cc42f099df6dc459c26
Lazar Conic <lazarconic@yahoo.com>
2025-12-30 12:54:32 +0100

    Added mail icon. Added cursor spotlight.

--- js/index.js
+++ js/index.js
@@ -22,4 +22,22 @@ document.addEventListener("DOMContentLoaded", () => {
   sections.forEach((section) => observer.observe(section));
 });
 
+// Get year
 document.getElementById("year").textContent = new Date().getFullYear();
+
+// Cursor spotlight effect
+let mouseX = 0;
+let mouseY = 0;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function updateSpotlight() {
+  document.body.style.setProperty("--mouse-x", mouseX + "px");
+  document.body.style.setProperty("--mouse-y", mouseY + "px");
+  requestAnimationFrame(updateSpotlight);
+}
+
+requestAnimationFrame(updateSpotlight);
